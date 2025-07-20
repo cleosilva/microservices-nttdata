4c80213fcfdd5d0b76684bbe77f9aecf47896d19
Cleo Silva <cleo_silva1906@hotmail.com>
2025-07-19 21:32:38 -0300

    feat(gateway): apply token validation filter per route instead of default-filters

--- api-gateway/src/main/java/cleosilva/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/cleosilva/api_gateway/config/GatewayConfig.java
@@ -6,13 +6,33 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class GatewayConfig {
     @Autowired
     private TokenValidationFilter tokenValidationFilter;
+
+    // Desabilita o filtro de segurança padrão do Spring Security para permitir o filtro customizado
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() //Permitir tudo, pois o filtro customizado fará o trabalho
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Use BCryptPasswordEncoder
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -26,14 +46,5 @@ public class GatewayConfig {
                 .build();
     }
 
-    // Desabilita o filtro de segurança padrão do Spring Security para permitir o filtro customizado
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().permitAll() //Permitir tudo, pois o filtro customizado fará o trabalho
-                );
-        return http.build();
-    }
+
 }
--- api-gateway/src/main/java/cleosilva/api_gateway/filter/TokenValidationFilter.java
+++ api-gateway/src/main/java/cleosilva/api_gateway/filter/TokenValidationFilter.java
@@ -4,9 +4,11 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class TokenValidationFilter implements GatewayFilter {
 
     private static final String AUTH_HEADER = "Authorization";
